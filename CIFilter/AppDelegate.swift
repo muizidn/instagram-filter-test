8601d3ab759828c3bcaa0ca425b84b10bc9f21f2
muizidn <muiz.idn@gmail.com>
2020-07-25 19:33:27 +0700

    Initial Commit

---
+++ CIFilter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CIFilter
+//
+//  Created by Muis on 25/07/20.
+//  Copyright © 2020 Muis. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CIFilter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CIFilter
+//
+//  Created by Muis on 25/07/20.
+//  Copyright © 2020 Muis. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
